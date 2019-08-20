4ec65bd2b4c4470af41d1ce57d9d905ee16580ff
emerihcelestin <emerih02@gmail.com>
2019-08-20 10:07:44 +0300

    view

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-20.times do 
- 	users = User.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, description: Faker::Lorem.paragraph(sentence_count: 3, supplemental: false, random_sentences_to_add: 4), email: Faker::Name.first_name+'@yopmail.com')
- end
+# 20.times do 
+#  	users = User.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, description: Faker::Lorem.paragraph(sentence_count: 3, supplemental: false, random_sentences_to_add: 4), email: Faker::Name.first_name+'@yopmail.com')
+#  end
 
 # user = User.first(10)
 # user_id =[]
@@ -16,19 +16,19 @@
 # end
 
 # 10.times do 
-# 	event = Event.create(start_date: Faker::Date.between(from: DateTime.now, to: Date.today), duration: 60.minute.to_i ,title: Faker::Music.album, description: Faker::Movie.quote, price: rand(1..1000), location: Faker::WorldCup.stadium , admin_id: user_id[rand(0..user_id.length)])
+# 	event = Event.create(start_date: Faker::Date.between(from: DateTime.now, to: Date.today+rand(1..50).day), duration: rand(60..320).minute.to_i ,title: Faker::Music.album, description: Faker::TvShows::GameOfThrones.quote, price: rand(1..1000), location: Faker::WorldCup.stadium , admin_id: user_id[rand(0..user_id.length)])
 # end	
-# user = User.last(10)
-# user_id =[]
-# user.each	do |user| 
-# user_id << user.id
-# end
-# event = Event.all
-# event_id =[]
-# event.each	do |event| 
-# event_id << event.id
-# end
+user = User.last(10)
+user_id =[]
+user.each	do |user| 
+user_id << user.id
+end
+event = Event.all
+event_id =[]
+event.each	do |event| 
+event_id << event.id
+end
 
-# 50.times do 
-# 	attendance = Attendance.create(stripe_customer_id: Faker::Stripe.valid_card, participant_id: user_id[rand(0..user_id.length)] , event_id: event_id[rand(0..event_id.length)] )
-# end	
+50.times do 
+	attendance = Attendance.create(stripe_customer_id: Faker::Stripe.valid_card, participant_id: user_id[rand(0..user_id.length)] , event_id: event_id[rand(0..event_id.length)] )
+end	
