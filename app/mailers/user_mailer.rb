9ffb4f0331d9844ac068b186482ea69dcff78ed0
emerihcelestin <emerih02@gmail.com>
2019-09-13 14:08:09 +0300

    dezsdsds

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,13 +1,11 @@
 class UserMailer < ApplicationMailer
+	default from: 'emerih01@gmail.com'
 	
 	 def welcome_email(user)
 
 		@user = user
-     courrier(
-       to: @user.email,
-       subject: 'Bienvenue chez nous !'
-       delivery_method_options: { api_key: '4c8bfa3f5d6084ce7fc18797098b439f' , secret_key: '1065521ae50c6f6a8ca6efd5d1ceeecf' }
-    )
-  end
- end
+		@url  = 'http://monsite.fr/login'
+		mail(to: @user.email, subject: 'Bienvenue chez nous !')
+  
+ 	end
 end
